21a2906514844e228f15a09a99056ce2e2a932d4
josephinex <josephinex21@gmail.com>
2017-10-07 22:56:47 +0300

    Add xml spring configuration

--- SpringXmlConfiguration2/src/main/java/Application.java
+++ SpringXmlConfiguration2/src/main/java/Application.java
@@ -1,9 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.CustomerService;
 import service.CustomerServiceImpl;
 
 public class Application {
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
+        //CustomerService service = new CustomerServiceImpl();
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext");
+
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
         System.out.println(service.findAll().get(0).getFirstName()); //sout
 
     }
--- SpringXmlConfiguration2/src/main/java/service/CustomerServiceImpl.java
+++ SpringXmlConfiguration2/src/main/java/service/CustomerServiceImpl.java
@@ -7,7 +7,12 @@ import repository.HibernateCustomerRepositoryImpl;
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+
+    private CustomerRepository customerRepository;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll(){
