141559b2b4a6562d2f370b1f2a8afa2fc648da0d
josephinex <josephinex21@gmail.com>
2017-10-08 10:27:59 +0300

    Java Spring Configuration Module

---
+++ SpringJavaConfiguration/src/main/java/BraveKnight.java
@@ -0,0 +1,12 @@
+public class BraveKnight implements Knight {
+
+    private Quest quest;
+
+    public BraveKnight(Quest quest){
+        this.quest = quest;
+    }
+
+    public void embarkOnQuest(){
+        quest.embark();
+    }
+}
